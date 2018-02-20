7057ab2b59375ee7e8cb9219651b8c663ebbfa4e
gonzalo <greco_gonzalo@hotmail.com>
2018-02-20 09:22:10 -0300

    while II

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,22 +1,39 @@
 function Mostrar()
 {
-
-	var contador = 0; 
-	var cantPos = 0;
-	var cantNeg = 0;
-	var cantCeros = 0;
+ 
+	var contPos = 0;
+	var contNeg = 0;
+	var contCeros = 0;
 	var sumaNeg = 0;
 	var sumaPos = 0;
-	var cantPares = 0;
+	var contPares = 0;
 	var respuesta = "si";
 
 	while(respuesta != "no")
 	{
 		var num = parseInt(prompt("Ingrese un numero: "));
+		
+		if (num > 0)
+		{
+			contPos ++;
+			sumaPos += num;
+		}
+
+		else if (num < 0)
+		{
+			contNeg ++;
+			sumaNeg -= num;
+		}
+
+		else
+			contCeros ++;
+
+		if (num % 2 == 0 && num != 0)
+			contPares ++;
+
 		respuesta = prompt("¿Desea continuar?");
 	}
 
-
-
+	document.write("Cantidad de ceros: ", contCeros, "<br>Cantidad de pares: ", contPares, "<br>Cantidad de positivos: ", contPos, "<br>Cantidad de negativos: ", contNeg, "<br>Suma positivos: ", sumaPos, "<br>Suma negativos: ", sumaNeg, "<br>Promedio positivos: ", sumaPos / contPos, "<br>Promedio negativos: ", sumaNeg / contNeg, "<br>Diferencia: ", sumaPos - sumaNeg);
 
 }//FIN DE LA FUNCIÓN
